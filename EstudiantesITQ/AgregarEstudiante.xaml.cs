240a4e6900b507573f39fff4a3aad6a507eaf663
CRIISTHI4N <gamers1235velez@gmail.com>
2023-01-11 11:25:41 -0500

    Agregar archivos de proyecto.

---
+++ EstudiantesITQ/AgregarEstudiante.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstudiantesITQ
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AgregarEstudiante : ContentPage
+    {
+
+        private const string Url = "http://10.20.23.200/itq/post.php";
+
+        public AgregarEstudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void insertar_Clicked(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                var parameters = new System.Collections.Specialized.NameValueCollection();
+                parameters.Add("codigo", entCodigo.Text);
+                parameters.Add("nombre", entNombre.Text);
+                parameters.Add("apellido", entApellido.Text);
+                parameters.Add("correo", entCorreo.Text);
+                parameters.Add("telefono", entTelefono.Text);
+
+                client.UploadValues(Url, "POST", parameters);
+                DisplayAlert("Completado", "Estudiante Registrado: " + entNombre + " " + entApellido, "Cerrar");
+                Limpiar();
+
+            } catch(Exception ex) 
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+                DisplayAlert("Error", "Se produjo un Errrrroooorr: ", "Cerrar");
+
+            }
+        }
+
+        public void Limpiar()
+        {
+            entCodigo.Text = string.Empty;
+            entNombre.Text = string.Empty;
+            entApellido.Text = string.Empty;
+            entCorreo.Text = string.Empty;
+            entTelefono.Text = string.Empty;
+        }
+
+        private async void btnRegreso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListadoEstudiantes());
+        }
+    }
+}
---
+++ EstudiantesITQ/ListadoEstudiantes.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstudiantesITQ
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListadoEstudiantes : ContentPage
+    {
+        private const string Url = "http://10.20.23.200/itq/post.php";
+
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Datos> _post;
+        public int codigo = -1;
+        public string nombre, apellido, correo, telefono;
+
+        private async void btnNuevoEstudiante_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AgregarEstudiante());
+        }
+
+        public ListadoEstudiantes()
+        {
+            InitializeComponent();
+            Get();
+        }
+
+        public async void Get()
+        {
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<Datos> post = 
+                    JsonConvert.DeserializeObject<List<Datos>>(content);
+                _post = new ObservableCollection<Datos>(post);
+                lstEstudiantes.ItemsSource = post;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+
+
+
+
+    }
+}
